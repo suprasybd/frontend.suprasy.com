2d8b886fb8d97fc0588a15a040361e81ae7e7001
malvee <m.alvee8141@gmail.com>
2024-02-11 01:06:21 +0600

    save

--- apps/customer/src/main.tsx
+++ apps/customer/src/main.tsx
@@ -1,16 +1,24 @@
+import { RouterProvider, createRouter } from '@tanstack/react-router';
 import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { routeTree } from './routeTree.gen';
 
-import App from './app/app';
+const router = createRouter({ routeTree });
+
+// Register the router instance for type safety
+declare module '@tanstack/react-router' {
+  interface Register {
+    router: typeof router;
+  }
+}
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <StrictMode>
+      <RouterProvider router={router} />
+    </StrictMode>
   </StrictMode>
 );

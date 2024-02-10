f7768bec5e71f7562e15e7c323928e1cde4f2d72
malvee <m.alvee8141@gmail.com>
2024-02-10 20:44:30 +0600

    setup in process

--- apps/web/src/app/app.tsx
+++ apps/web/src/app/app.tsx
@@ -1,11 +1,13 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.scss';
 
+import { Ui } from '@frontend.suprasy.com/ui';
 import NxWelcome from './nx-welcome';
 
 export function App() {
   return (
     <div>
+      <Ui />
       <NxWelcome title="web" />
     </div>
   );

66a249c44292ec2ec1c1aebcb18f5f8076033e26
Alvee <M.Alvee8141@gmail.com>
2024-04-28 22:01:04 +0600

    updates

--- apps/customer/src/pages/shipping/components/Area.zod.ts
+++ apps/customer/src/pages/shipping/components/Area.zod.ts
@@ -3,6 +3,6 @@
 import { z } from 'zod';
 
 export const areaSchema = z.object({
-  Area: z.string().min(2).max(50),
+  Area: z.string().min(2).max(200),
   Cost: z.coerce.number(),
 });
--- apps/customer/src/pages/shipping/components/shipping-method/Method.zod.ts
+++ apps/customer/src/pages/shipping/components/shipping-method/Method.zod.ts
@@ -3,6 +3,6 @@
 import { z } from 'zod';
 
 export const methodSchema = z.object({
-  DeliveryMethod: z.string().min(2).max(50),
+  DeliveryMethod: z.string().min(2).max(200),
   Cost: z.coerce.number(),
 });

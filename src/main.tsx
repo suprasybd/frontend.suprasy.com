3202e30ed5863b25485c08f5bfb5c47ee588057c
Alvee <M.Alvee8141@gmail.com>
2024-11-09 21:28:59 +0600

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,15 @@
 import { StrictMode, Suspense } from 'react';
 import * as ReactDOM from 'react-dom/client';
 import App from './app';
+import { LoaderMain } from './components/Loader/Loader';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  // <StrictMode>\
-  <Suspense fallback={<p>loading</p>}>
+  // <StrictMode>
+  <Suspense fallback={<LoaderMain />}>
     <App />
   </Suspense>
-
   // </StrictMode>
 );
